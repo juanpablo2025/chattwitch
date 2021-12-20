9c7badf75dbefdff8b289c9647d55e328da22152
Juan <juanpablo_1810@hotmail.com>
2021-12-20 17:08:06 -0500

    server index

--- server/index.js
+++ server/index.js
@@ -32,3 +32,31 @@ app.post('/signup', async (req, res) => {
 	}
 })
 
+//login 
+app.post('/login', async (req, res) => {
+	try {
+		const { username, password } = req.body
+		const client = connect(process.env.API_KEY_SECTRE, process.env.APP_ID)
+		const chatClient = StreamChat.getInstance(process.env.API_KEY, process.env.API_SECRET)
+		const { users } = await chatClient.queryUsers({ name: username })
+
+		if (!users.length) return res.json(400).json({ message: 'User does not exist' })
+
+		const success = await bcrypy.compare(password.users[0].hashedPassword)
+		const token = client.createUserToken(users[0].id)
+		const confirmedName = users[0].id
+
+
+		if (success) {
+			res.status(200).json({ token, username: confirmedName, userdId })
+
+		} else {
+			res.status(200).json({ message: 'login failed' })
+		}
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ message: error })
+	}
+})
+
+app.listen(PORT, () => console.log('Server runnin on PORT' + PORT))
