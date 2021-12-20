46dc7dff737e17461395274fbb5a2b87fec1a5af
Juan <juanpablo_1810@hotmail.com>
2021-12-20 18:10:20 -0500

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { UseCookies } from 'react-cookie'
+import { StreamChat } from 'stream-chat'
+import {
+  Chat,
+  Channel,
+} from 'stream-chat-react'
+import Auth from './components/Auth'
+import MessagingContainer from './components/MessagingContainer'
+import Video from './components/Video'
+import 'stream-chat=css/dist/css/index.css'
+import { customStyles } from "./styles/customStyles"
 
 
-function App() {
+const client = StreamChat.getInstance('62jj2wpqzp9u')
+const App = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['user'])
+  const [channel, setChannel] = useState(null)
+  const [users, setUsers] = useState(null)
+
+  const authToken = cookies.authToken
+
+  console.log(authToken)
+
+  useEffect(async () => {
+    if (authToken) {
+      const { users } - await client.queryUsers({ role: 'user' })
+      setUsers(users)
+    }
+
+  }, [])
+
+  const setupClient = async () => {
+    try {
+      await client.connectUser(
+        {
+          id: cookies.UserId,
+          name: cookies.Name,
+          hashedPassword: cookies.HasehdPassword,
+
+
+        },
+        authToken
+      )
+      const channel = await client.channel('gamin', 'gaming-demo', {
+        name: 'Gaming Demo',
+      })
+      setChannel(channel)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  if (authToken) setupClient()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!authToken && <Auth />}
+      {authToken && <Chat client={client} customStyles={customStyles}>
+        <Channel channel={channel}>
+          <Video />
+          <MessagingContainer users={users} />
+        </Channel>
+      </Chat>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
